d5b3d357897f1303656ffe4be58f332a8111ccd6
skruseman <skruseman@gmail.com>
2026-05-11 20:35:55 +0200

    working on str and repr for identifiers

--- identifier.py
+++ identifier.py
@@ -3,15 +3,44 @@ from __future__ import annotations
 from enum import Enum
 
 
-class Id (object):
-    def __init__(self, val: int = 0, num_bytes: int = 2):
-        self.val = val
+class Id:
+
+    def __init__(self, val: int, num_bytes: int = 2):
         self.num_bytes = num_bytes
-    def to_bytes(self):
+        if val <= 0:
+            raise ValueError("Not a positive integer value")
+        if val >= (256 ** self.num_bytes):
+            raise ValueError("Exceeds max value")
+        self.val = val
+
+    def to_bytes(self) -> bytes:
         return self.val.to_bytes(length=self.num_bytes)
+
+    def to_hex(self) -> str:
+        return self.to_bytes().hex()
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__ + str(self)
+
+    def __str__(self) -> str:
+        return str(self.val)
 
 
 class SubId(Id):
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__ + str(self)
+
+    def __str__(self) -> str:
+        return str(tuple(self._list_values()))
+
+    def _list_values(self) -> list[int]:
+        if isinstance(self.sup, SubId):
+            rv = self.sup._list_values()
+            rv.append(self.val)
+            return rv
+        return [self.sup.val, self.val]
+
     class Level(Enum):
         ONE = 1
         TWO = 2
@@ -36,9 +65,9 @@ class SubId(Id):
 
         last_id = SubId._last_id_by_level[self.level]
         val = last_id + 1
-        assert val <= self.LEVEL_SIZES[self.level]["MAX_value"], ValueError("Exceeds max value")
+        assert val <= self.LEVEL_SIZES[self.level]["max_value"], ValueError("Exceeds max value")
         super().__init__(val, num_bytes=SubId.LEVEL_SIZES[self.level]["num_bytes"])
         SubId._last_id_by_level[self.level] = val
 
-    def to_bytes(self):
+    def to_bytes(self) -> bytes:
         return self.sup.to_bytes() + super().to_bytes()
