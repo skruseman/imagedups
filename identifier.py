92ed537f8617809b0a40761a126d4000ea3b0292
skruseman <skruseman@gmail.com>
2026-05-12 15:51:48 +0200

    remove dual definition of number of bytes per CompositeId level

--- identifier.py
+++ identifier.py
@@ -57,32 +57,36 @@ class CompositeId(Id):
         ONE = 1
         TWO = 2
 
-    LEVEL_SIZES = {
-        Level.ONE: {"num_bytes": 2, "max_value": 256**2 - 1},
-        Level.TWO: {"num_bytes": 4, "max_value": 256**4 - 1},
+    NUM_BYTES = {
+        Level.ONE: 2,
+        Level.TWO: 4,
     }
+
+    @staticmethod
+    def _max_value(level: CompositeId.Level) -> int:
+        return 256**CompositeId.NUM_BYTES[level] - 1
 
     _last_id_by_level = {
         Level.ONE: 0,
         Level.TWO: 0,
     }
 
-    def __init__(self, sup: Id):
-        self.sup = sup
-        if isinstance(sup, CompositeId):
-            assert sup.level == CompositeId.Level.ONE, "Nesting beyond two sub-levels is not supported"
+    def __init__(self, base: Id):
+        self.base = base
+        if isinstance(base, CompositeId):
+            assert base.level == CompositeId.Level.ONE, "Nesting beyond two sub-levels is not supported"
             self.level = CompositeId.Level.TWO
-        else:  # sup is of class: Id
+        else:  # base is of class: Id
             self.level = CompositeId.Level.ONE
 
         last_id = CompositeId._last_id_by_level[self.level]
         val = last_id + 1
-        assert val <= self.LEVEL_SIZES[self.level]["max_value"], ValueError("Exceeds max value")
-        super().__init__(val, num_bytes=CompositeId.LEVEL_SIZES[self.level]["num_bytes"])
+        assert val <= self._max_value(self.level), ValueError("Exceeds max value")
+        super().__init__(val, num_bytes=CompositeId.NUM_BYTES[self.level])
         CompositeId._last_id_by_level[self.level] = val
 
     def to_bytes(self) -> bytes:
-        return self.sup.to_bytes() + super().to_bytes()
+        return self.base.to_bytes() + super().to_bytes()
 
     def __repr__(self) -> str:
         return self.__class__.__name__ + str(self)
@@ -91,6 +95,6 @@ class CompositeId(Id):
         return str(tuple(self._list_values()))
 
     def _list_values(self) -> list[int]:
-        if isinstance(self.sup, CompositeId):
-            return self.sup._list_values() + [self.val]
-        return [self.sup.val, self.val]
+        if isinstance(self.base, CompositeId):
+            return self.base._list_values() + [self.val]
+        return [self.base.val, self.val]
