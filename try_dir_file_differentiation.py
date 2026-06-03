bd5d3e9d383daaf127b5acd57dff3f1f9d8f0782
skruseman <skruseman@gmail.com>
2026-06-03 20:13:50 +0200

    Incorporated static typing proof improvements using generics

--- try_dir_file_differentiation.py
+++ try_dir_file_differentiation.py
@@ -1,47 +1,58 @@
+""" Demonstrates type generics ! """
+
 from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import Generic, Iterable, TypeVar, cast
+
+T = TypeVar('T')
 
 
 the_items = ['a', 10, 11, 'b', 12]
 
 
-class ItemHandler(ABC):
+class ItemHandler(ABC, Generic[T]):
 
     @abstractmethod
-    def print(self, item: str|int):
+    def print(self, item: T):
         pass
 
-class StrHandler(ItemHandler):
+class StrHandler(ItemHandler[str]):
 
     def print(self, item: str):
         print(f'String {item}')
 
-class IntHandler(ItemHandler):
+class IntHandler(ItemHandler[int]):
 
     def print(self, item: int):
         print(f'Int {item}')
 
 
-def _get_handler(item) -> ItemHandler:
+def _get_handler(item) -> StrHandler | IntHandler:
     if isinstance(item, str):
         return StrHandler()
-    else:
+    if isinstance(item, int):
         return IntHandler()
+    raise TypeError(f"Unsupported item type: {type(item).__name__}")
 
 
-def do_items(items: List[int|str]):
+def do_items(items: Iterable[int | str]):
+    """ Allows mixed input."""
     _do_items(items)
 
-def do_strs(strs: List[str]):
+def do_strs(strs: Iterable[str]):
     _do_items(strs, StrHandler())
 
-def do_ints(ints: List[int]):
+def do_ints(ints: Iterable[int]):
     _do_items(ints, IntHandler())
 
-def _do_items(items: List[int|str], handler: Optional[StrHandler|IntHandler] = None):
-    for item in items:
-        handler_ = handler if handler else _get_handler(item)
-        handler_.print(item)
+def _do_items(items: Iterable[int | str], handler: StrHandler | IntHandler | None = None):
+    """In this design static typing won't help me prevent cases where a type
+    specific handler is passed while the input contains items of a different type."""
+    with open('some_file', 'w') as f:
+        for item in items:
+            handler_ = handler or _get_handler(item)
+            cast(ItemHandler[int | str], handler_).print(item)
+
+            f.write(f'{item}\n')
 
 
 do_items(the_items)
